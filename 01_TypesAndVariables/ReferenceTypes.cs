1b3f4f7ea0728c77b4dd54072430f4f06cbf7154
JacobBrown4 <68622976+JacobBrown4@users.noreply.github.com>
2021-10-26 12:20:29 -0400

    Fixed bot maybe

--- 01_TypesAndVariables/ReferenceTypes.cs
+++ 01_TypesAndVariables/ReferenceTypes.cs
@@ -58,7 +58,7 @@ namespace _01_TypesAndVariables
             listOfStrings.Add("string");
             listOfStrings.Add("23221");
 
-            //Queues, We will talk about queues
+            //Queues
         }
     }
 }
