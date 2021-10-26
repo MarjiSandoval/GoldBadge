a9d7d5e62eb41c7a8f206e52d12dd94ee3bb8a45
JacobBrown4 <68622976+JacobBrown4@users.noreply.github.com>
2021-10-26 16:28:55 -0400

    Loops

---
+++ 03_Conditionals/Switch.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace _03_Conditionals
+{
+    [TestClass]
+    public class Switch
+    {
+        [TestMethod]
+        public void SwitchCases()
+        {
+            int input = 1;
+
+            switch (input)
+            {
+                case 1:
+                    Console.WriteLine("Hello!");
+                    break;
+                case 2:
+                    Console.WriteLine("What are you doing?");
+                    break;
+                default:
+                    Console.WriteLine("This is a default statement");
+                    break;
+            }
+
+            string age = "37";
+
+            switch (age)
+            {
+                case "18":
+                    // code for 18
+                    break;
+                case "32":
+                    // code for 32
+                    break;
+                case "33":
+                    break;
+                case "34":
+                case "35":
+                case "36":
+                    // Code for mid 30's
+                    break;
+                case "37":
+                case "Thirty seven":
+                case "3 7":
+                case "Thirtyseven":
+                    // code for 37
+                    break;
+                default:
+                    break;
+                    // Ineffecient for ranges or vague conditions.
+            }
+        }
+    }
+}
---
+++ 03_Conditionals/Ternaries.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace _03_Conditionals
+{
+    [TestClass]
+    public class Ternaries
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int age = 31;
+
+            // variable = (Condition/Boolean) ? trueResult : falseResult;
+            bool isAdult = (age > 17) ? true : false;
+
+            int numOne = 10;
+
+            int numTwo = (numOne == 10) ? 30 : 20;
+            Console.WriteLine(numTwo);
+
+            // As an if else
+            if (numOne == 10)
+                numTwo = 30;
+            else
+                numTwo = 20;
+
+
+        }
+    }
+}
