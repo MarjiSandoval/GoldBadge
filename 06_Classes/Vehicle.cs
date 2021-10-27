d00f3c0901b3472738bd79580b121f54437ae841
Terry Brown <Jhondoe256@yahoo.com>
2021-10-27 15:50:22 -0400

    second push

--- 06_Classes/Vehicle.cs
+++ 06_Classes/Vehicle.cs
@@ -9,8 +9,35 @@ namespace _06_Classes
     public enum VehicleType { Car, Truck, Van , Motorcycle, Spaceship, Plane, Boat}
 
     //Abstraction
+    //fields -> private backing field..
+    //Constructors -> used to make objects on the fly
+    //Properties -> attributes of an object(class)
+    //Methods-> are the actions that a object does
     public class Vehicle
     {
+        //ctor ->tab ->tab
+        public Vehicle()
+        {
+            LeftIndicator = new Indicator();
+            RightIndicator = new Indicator();
+        }
+        public Vehicle(string make, string model, double milage)
+        {
+            Make = make;
+            Model = model;
+            Milage = milage;
+            LeftIndicator = new Indicator();
+            RightIndicator = new Indicator();
+        }
+        public Vehicle(string make, string model, double milage,VehicleType vehicleType)
+        {
+            Make = make;
+            Model = model;
+            Milage = milage;
+            TypeOfVehicle = vehicleType;
+            LeftIndicator = new Indicator();
+            RightIndicator = new Indicator();
+        }
         //1 Access Modifier 
         //2 type -> type that the property can hold
         //3 name -> name of the property
@@ -26,6 +53,10 @@ namespace _06_Classes
 
         public Indicator LeftIndicator { get; set; }
         //we need a Right Indicator....
+        public Indicator RightIndicator { get; set; }
+
+
+
 
         //Methods-> Actions...
         public void TurnOn()
@@ -40,12 +71,33 @@ namespace _06_Classes
             Console.WriteLine("You turned off the vehicle.");
         }
 
+        public override string ToString()
+        {
+            return $"{Make} {Model} {Milage} {TypeOfVehicle}";
+        }
     }
 
     public class Indicator 
     {
         //we want a bool isFlashing
+        //you can only write to this w/n the 'scope' of this
+        //class
+        public bool IsFlashing { get; private set; }
 
         //methods: TurnOn() and TurnOff()
+        //first method:
+        public void TurnOn()
+        {
+            IsFlashing = true;
+            Console.WriteLine("Lights are on.");
+        }
+
+        //second method:
+        public void TurnOff()
+        {
+            IsFlashing = false;
+            Console.WriteLine("Lights are off.");
+        }
+
     }
 }
