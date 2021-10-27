c9651cac77876b1cd6779110740b99ab2ac6f434
JacobBrown4 <68622976+JacobBrown4@users.noreply.github.com>
2021-10-27 09:14:45 -0400

    Setup challenge

---
+++ 04_Loops/LoopChallenge.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace _04_Loops
+{
+    [TestClass]
+    public class LoopChallenge
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
