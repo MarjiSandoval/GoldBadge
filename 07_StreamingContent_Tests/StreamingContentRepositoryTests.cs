2d201ff371e7c8a85c38ec4b2c83f166947a54be
JacobBrown4 <68622976+JacobBrown4@users.noreply.github.com>
2021-11-01 13:36:08 -0400

    Repository Testing

--- 07_StreamingContent_Tests/StreamingContentRepositoryTests.cs
+++ 07_StreamingContent_Tests/StreamingContentRepositoryTests.cs
@@ -37,5 +37,39 @@ namespace _07_StreamingContent_Tests
             // Assert
             Assert.IsTrue(directoryHasContent);
         }
+
+        private StreamingContentRepository _repo;
+        private StreamingContent _content;
+
+        [TestInitialize]
+        public void Arrange()
+        {
+            _repo = new StreamingContentRepository();
+            _content = new StreamingContent("Rubber", "A car tire comes to life with the power to make people explode", MaturityRating.R, 7.6, GenreType.Thriller);
+
+            _repo.AddContentToDirectory(_content);
+        }
+
+        [TestMethod]
+        public void GetByTitle_ShouldReturnCorrectContent()
+        {
+            // Arrange
+            // Arrange is done in the test initialize
+            // Act
+            StreamingContent foundContent = _repo.GetContentByTitle("Rubber");
+            // Assert
+            Assert.AreEqual(_content, foundContent);
+        }
+
+        [TestMethod]
+        public void UpdateExistingContent_ShouldReturnTrue()
+        {
+            // Arrange
+            StreamingContent newContent = new StreamingContent("Rubber", "A car tire comes to life with the power to make people explode.", MaturityRating.R, 10, GenreType.Thriller);
+            // Act
+            bool updateResult = _repo.UpdateExistingContentByTitle("Rubber", newContent);
+            // Assert
+            Assert.IsTrue(updateResult);
+        }
     }
 }
