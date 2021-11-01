db05ee1a435612c384a4fa8f97a0710c8add3b0f
JacobBrown4 <68622976+JacobBrown4@users.noreply.github.com>
2021-11-01 10:33:10 -0400

    Started streaming content

---
+++ 07_StreamingContent_Tests/StreamingContentTests.cs
@@ -0,0 +1,45 @@
+﻿using _07_StreamingContent_Repository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace _07_StreamingContent_Tests
+{
+    [TestClass]
+    public class StreamingContentTests
+    {
+        // Basic functionality
+        [TestMethod]
+        public void SetTitle_ShouldGetCorrectString()
+        {
+            // Arrange
+            StreamingContent content = new StreamingContent();
+
+            content.Title = "Toy Story";
+            // ACt
+            string expected = "Toy Story";
+            string actual = content.Title;
+            // Assert
+            Assert.AreEqual(expected, actual);
+             
+        }
+        // Test IsFamilyFriendly
+        [DataTestMethod]
+        [DataRow(MaturityRating.G,true)]
+        [DataRow(MaturityRating.TV_G,true)]
+        [DataRow(MaturityRating.R,false)]
+        [DataRow(MaturityRating.TV_MA, false)]
+        public void SetMaturityRating_ShouldGetCorrectIsFamilyFriendlyRating(MaturityRating maturity,bool expectedFamilyFriendly)
+        {
+            // Triple A Paradigm
+
+            // Arrange => Set the stage for testing
+            StreamingContent content = new StreamingContent("Some title", "Some description", maturity, 3.5, GenreType.Horror);
+            // Act => Execute code we run
+            bool actual = content.IsFamilyFriendly;
+            bool expected = expectedFamilyFriendly;
+            // Assert => Call our assertions
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
