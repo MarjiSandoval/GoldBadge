346dd5cc631bcd6ca1513a4b4eb79ec0f4c06122
Marjorie Sandoval <Marjorie.Sandoval74@gmail.com>
2021-11-02 10:46:29 -0400

    calculator challenge added

---
+++ CalculatorChallenge/Calculator.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorChallenge
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            int sum = a + b;
+            return sum;
+
+        }
+        public int Subtract(int a, int b)
+        {
+            int product = b - a;
+            return product;
+        }
+        public int multiply(int a, int b)
+        {
+            int answer = a * b;
+            return answer;
+        }
+        public int divide(int a, int b)
+        {
+            int quotient = a / b;
+            return quotient;
+
+        }
+        public decimal divide(decimal a, decimal b)
+        {
+            decimal quotient = a / b;
+            return quotient;
+        }
+        public decimal Add(decimal a, decimal b)
+        {
+            decimal sum = a + b;
+            return sum;
+
+        }
+        public decimal Subtract(decimal a, decimal b)
+        {
+            decimal product = b - a;
+            return product;
+        }
+        public decimal multiply(decimal a, decimal b)
+        {
+            decimal answer = a * b;
+            return answer;
+        }
+        //This is how you add multiple numbers
+        public decimal Add(params decimal[] numbers)
+        {
+            //We have to start somewhere...
+            //so we will start at 0,
+            //but to use this I need it in a container 'startingVal'
+            decimal startingVal = 0;
+            if (numbers.Length > 0)
+            {
+                startingVal += numbers[0];
+            }
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                startingVal += numbers[i];
+            }
+            return startingVal;
+        }
+        public decimal Subtract(params decimal[] numbers)
+        {
+            //We have to start somewhere...
+            //so we will start at 0,
+            //but to use this I need it in a container 'startingVal'
+            decimal startingVal = 0;
+            if (numbers.Length > 0)
+            {
+                startingVal -= numbers[0];
+            }
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                startingVal += numbers[i];
+            }
+            return startingVal;
+        }
+        public decimal multiply(params decimal[] numbers)
+        {
+            //We have to start somewhere...
+            //so we will start at 0,
+            //but to use this I need it in a container 'startingVal'
+            decimal startingVal = 0;
+            if (numbers.Length > 0)
+            {
+                startingVal *= numbers[0];
+            }
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                startingVal += numbers[i];
+            }
+            return startingVal;
+        }
+        public decimal divide(params decimal[] numbers)
+        {
+            //We have to start somewhere...
+            //so we will start at 0,
+            //but to use this I need it in a container 'startingVal'
+            decimal startingVal = 0;
+            if (numbers.Length > 0)
+            {
+                startingVal *= numbers[0];
+            }
+            for (int i = 1; i < numbers.Length; i++)
+            {
+               if (i != 0)
+                {
+                    startingVal /= numbers[i];
+                }
+            }
+            return startingVal;
+        }
+        //if you want to take the fraction approach https://www.codeproject.com/articles/11971/fractions-in-c
+
+        public string Add_FractionFormat(decimal a, decimal b)
+        {
+            decimal value =a +_ b
+            string toFraction = $"{a}/{b}";
+            return toFraction;
+
+
+        }
+        public string Percent(double a, double b)
+        {
+            double c = a / b;
+            c *= 100;
+            return $"{c}%";
+        }
+    }
+
+}
+
---
+++ CalculatorChallenge/CalculatorChallengeTest.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CalculatorChallenge
+{
+    [TestClass]
+    public class CalculatorChallengeTest
+    {
+        private Calculator _calc;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _calc = new Calculator();
+        }
+        [TestMethod]
+        public void AddTwoNumbers_ReturnCorrectAnswer()
+        {
+            int actual = _calc.Add(5, 6);
+            int expected = 11;
+
+            Assert.AreEqual(expected, actual);
+
+        }
+        [TestMethod]
+        public void SubtractTwoNumbers_RetrunCorrectAnswer()
+        {
+            int actual = _calc.Subtract(9, 15);
+            int expected = 6;
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void MultiplyTwoNumber_ReturnCorrectAnswer()
+        {
+            int actual = _calc.multiply(9, 9);
+            int expected = 81;
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void DivideTwoNumbers_ReturnCorrectAnswer()
+        {
+            int actual = _calc.divide(50, 5);
+            int expected = 10;
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void AddTwoDecimals_ReturnCorrectSum()
+        {
+            decimal actual = _calc.Add(0.75m, 15.92m);
+            decimal expected = 16.67m;
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void SubtractDecimals_ReturnCorrectProduct()
+        {
+            decimal actual = _calc.Subtract(0.75m, 15.92m);
+            decimal expected = 15.17m;
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void MultiplyDecimals_ReturnCorrectAnswer()
+        {
+            decimal actual = _calc.multiply(0.75m, 15.92m);
+            decimal expected = 11.94m;
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void DivideTwoDecimals_ReturnCorrectQuotient()
+        {
+            decimal actual = _calc.Add(0.75m, 15.92m);
+            decimal expected = 16.67m;
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Add_PassInAnything_Decimal()
+        {
+            //Act
+            decimal actual = _calc.Add(1.1m, 1.2m, 1.3m);
+            decimal expected = 3.6m;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }  
+}
